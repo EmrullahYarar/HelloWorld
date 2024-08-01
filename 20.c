ecb8536eb79c93fc91a0e83431f8de540b258e9f
EmrullahYarar <147341379+EmrullahYarar@users.noreply.github.com>
2024-08-01 17:15:31 +0300

    Add files via upload

---
+++ 20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("%c", 72); //H
+    printf("%c", 101); //e
+    printf("%c", 108); //l
+    printf("%c", 108); //l
+    printf("%c ", 111); //o
+
+    printf("%c", 87); //W
+    printf("%c", 111); //o
+    printf("%c", 114); //r
+    printf("%c", 108); //l
+    printf("%c", 100); //d
+    printf("%c\n", 33); //!
+
+    return 0;
+}
