4417b4599cc008cf96b3db3e3da723ed091335ca
Emrullah Yarar <147341379+EmrullahYarar@users.noreply.github.com>
2024-11-06 13:54:08 +0300

    Add files via upload

---
+++ 25.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    char str[12] = "Hello World!";
+    char *ptr = str;
+
+    printf("%s\n", ptr);
+
+    return 0;
+}
---
+++ 26.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    char *ptr = "Hello World!";
+    printf("%s\n", ptr);
+
+    return 0;
+}
